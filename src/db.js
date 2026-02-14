8d52926844f99d24c0819cd16b8f256c4a4b27dc
Jrb You <jrb.youssef@gmail.com>
2026-02-14 16:33:44 +0100

    chore(fix VCE of mongoose): bump to 6.13.8

--- src/db.js
+++ src/db.js
@@ -1,8 +1,5 @@
 import mongoose from 'mongoose';
 
 export default async function db () {
-  await mongoose.connect(process.env.DB_STRING, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  await mongoose.connect(process.env.DB_STRING)
 };
